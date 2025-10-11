35bef16c8ca16da47d0deaecf40340fc5a5529d9
Ethan Leigh <ethanl22702@gmail.com>
2025-10-11 16:01:18 -0400

    initial commit

---
+++ src/main/java/com/Kosovo/Cradle/Leaderboard/KosovoCradleLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.Kosovo.Cradle.Leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosovoCradleLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosovoCradleLeaderboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kosovo/Cradle/Leaderboard/KosovoCradleLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kosovo.Cradle.Leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosovoCradleLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
